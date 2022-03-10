931cc14d8179d10582019792fd5e906d7a82b0ed
Rathod Shubham <shubhamrathod664@gmail.com>
2022-03-10 18:57:53 +0530

    new change

---
+++ Reddit Clone/script.js
@@ -0,0 +1,19 @@
+function upvote()
+{
+    let votes = document.getElementById('votes');
+    let count = parseInt(votes.innerHTML);
+    count++;
+    votes.innerHTML = count;
+
+}
+
+function downvote()
+{
+    let votes = document.getElementById('votes');
+    let count = parseInt(votes.innerHTML);
+    count--;
+    votes.innerHTML = count;
+
+}
+
+
